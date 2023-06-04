036a06d1abe5570c951218357ab8904e8169c1b1
lminoru <leo.minoru@hotmail.com>
2023-06-04 04:18:33 -0300

    filtering by Curso working

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -8,16 +8,14 @@
       </v-dialog>
     </template>
     <template v-if="defesas.length !== 0">
-      <!-- Coluna com input para filtragem -->
+      <!-- input para filtragem por nome -->
       <v-text-field type="text" v-model="search" append-icon="mdi-magnify" label="Digite um nome"
         single-line></v-text-field>
 
       <!-- Coluna com radio buttons para filtragem -->
-      <template>
-        <v-radio-group v-model="filters.curso" row>
-          <v-radio v-for="c in cursoOptions" :key="c.value" :label="c.text" :value="c.value"></v-radio>
-        </v-radio-group>
-      </template>
+      <v-radio-group v-model="filters.curso" row>
+        <v-radio v-for="c in cursoOptions" :key="c.text" :label="c.text" :value="c.value"></v-radio>
+      </v-radio-group>
 
       <v-data-table :headers="headers" :items="defesas" class="elevation-1" :loading="loading" :search="search">
 
@@ -44,7 +42,15 @@ export default {
       cnt: 0,
       search: '',
       headers: [
-        { text: 'Curso', value: 'Curso', filterable: false },
+        {
+          text: 'Curso',
+          value: 'Curso',
+          filter: value => {
+            if (!this.filters.curso) return true
+
+            return value === this.filters.curso
+          }
+        },
         { text: 'Programa', value: 'Programa', filterable: false },
         { text: 'Ordem', value: 'Ordem', filterable: false },
         { text: 'Nome', value: 'Nome' },
@@ -60,62 +66,51 @@ export default {
       },
       cursoOptions: [
         { text: 'Mestrado', value: 'Mestrado' },
-        { text: 'Doutorado', value: 'Doutorado' }
+        { text: 'Doutorado', value: 'Doutorado' },
+        { text: 'DD', value: 'DD' },
+        { text: 'Nenhum', value: '' }
       ]
-    }
-  },
-  created() {
-    this.loadDefesas()
-  },
-  methods: {
-    loadDefesas() {
-      this.loading = true
-      const url = 'http://thanos.icmc.usp.br:4567/api/v1/defesas'
-      fetch(url)
-        .then((data) => (data.json()))
-        .then((response) => {
-          this.defesas = this.formatItems(response.items)
-          this.loading = false
-        })
     },
-    formatCourse(courseString) {
-      if (courseString === 'ME') {
-        return 'Mestrado'
-      } else if (courseString === 'DO') {
-        return 'Doutorado'
-      }
-      return courseString
+      created() {
+      this.loadDefesas()
     },
-    formatDate(dateString) {
-      // Implemente a lógica para formatar a data no formato DD/MM/AAAA
-      const [year, month, day] = dateString.split('-')
-      const date = `${day}/${month}/${year}`
-      return date
-    },
-    formatDateForSorting(dateString) {
-      const [day, month, year] = dateString.split('/')
-      const isoDate = `${year}-${month}-${day}`
-      return isoDate
-    },
-    formatItems(data) {
-      return data.map((item) => ({
-        ...item,
-        Data: this.formatDateForSorting(item.Data),
-        Curso: this.formatCourse(item.Curso)
-      }))
-    }
-  },
-  computed: {
-    customFilter() {
-      return (value, search, item) => {
-        // Filtro para a coluna "Nome"
-        const nomeMatch = item.nome.toLowerCase().includes(search.toLowerCase())
-        // Filtro para a coluna "Curso"
-        const cursoMatch = item.curso === search
-
-        return nomeMatch && cursoMatch
+    methods: {
+      loadDefesas() {
+        this.loading = true
+        const url = 'http://thanos.icmc.usp.br:4567/api/v1/defesas'
+        fetch(url)
+          .then((data) => (data.json()))
+          .then((response) => {
+            this.defesas = this.formatItems(response.items)
+            this.loading = false
+          })
+      },
+      formatCourse(courseString) {
+        if (courseString === 'ME') {
+          return 'Mestrado'
+        } else if (courseString === 'DO') {
+          return 'Doutorado'
+        }
+        return courseString
+      },
+      formatDate(dateString) {
+        // Implemente a lógica para formatar a data no formato DD/MM/AAAA
+        const [year, month, day] = dateString.split('-')
+        const date = `${day}/${month}/${year}`
+        return date
+      },
+      formatDateForSorting(dateString) {
+        const [day, month, year] = dateString.split('/')
+        const isoDate = `${year}-${month}-${day}`
+        return isoDate
+      },
+      formatItems(data) {
+        return data.map((item) => ({
+          ...item,
+          Data: this.formatDateForSorting(item.Data),
+          Curso: this.formatCourse(item.Curso)
+        }))
       }
     }
   }
-}
 </script>
